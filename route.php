82ad92c999aa24a3d0aa76658145e0e8937da220
Alf Eaton <alf@hubmed.org>
2012-01-13 15:01:28 +0000

    Tidying.

--- route.php
+++ route.php
@@ -1,104 +1,103 @@
 <?php
 
 function route($request){
-  try {
-    switch ($request->type){
-      case '':
-        if ($_COOKIE['oauth-access']){
-          header('Location: library');
+  switch ($request->type){
+    case '':
+      if ($_COOKIE['oauth-access']){
+        header('Location: library');
+        exit();
+      }
+      return array(array(), 'intro');
+      
+    case 'logout':
+      MendeleyOAuth::logout();
+      exit();
+    
+    default:
+      return array(array(), 'index');
+
+    case 'auth':
+      try {
+        $data = Mendeley::profile('me');
+      }
+      catch (HTTPException $e){
+        if ($_GET['authorise']){     
+          MendeleyOAuth::fetch_access_token();
+          header('Location: ' . URL . 'auth');
           exit();
         }
-        return array(array(), 'intro');
-        
-      case 'logout':
-        MendeleyOAuth::logout();
-        exit();
+      }
+      return array($data, 'auth');
       
-      default:
-        return array(array(), 'index');
+    case 'importer':
+      return array(array(), '../importer/templates/index');
+      
+    case 'importer-sidebar':
+      return array(array(), '../importer/templates/sidebar');
+      
+    case 'importer-extractors':
+      require __DIR__ . '/importer/lib/Importer.php';
+      $scripts = Importer::selectScripts($_REQUEST['host']);
+			return array(array_filter(array_map(array('Importer', 'loadExtractorScript'), $scripts)));
 
-      case 'auth':
-        try {
-          $data = Mendeley::profile('me');
-        }
-        catch (HTTPException $e){
-          if ($_GET['authorise']){     
-            MendeleyOAuth::fetch_access_token();
-            header('Location: ' . URL . 'auth');
-            exit();
+    case 'api':
+      $type = array_shift($request->parts);
+      switch ($type){
+        case 'profile':
+          return array(Mendeley::profile('me'));
+          
+        case 'folders':
+          return array(Mendeley::library_folders(), 'folders');
+
+        case 'groups':
+          return array(Mendeley::library_groups());
+
+        case 'catalog':
+          $id = array_shift($request->parts);
+          if ($id) return array(Mendeley::document($id));
+          if (!$_GET['page']) $_GET['page'] = 0;
+          if (!$_GET['n']) $_GET['n'] = 20;
+          if (!$_GET['q']) throw new HTTPException(404, 'Not Found');
+          return array(Mendeley::search('documents', $_GET['q'], $_GET['page'], $_GET['n']), 'documents');
+          
+        case 'documents':
+          $id = array_shift($request->parts);
+          switch ($_SERVER['REQUEST_METHOD']){
+            case 'GET':
+              return get_documents($id, $request);
+        
+            case 'POST':
+              return post_documents($id, $request);
+
+            default:
+              header('Allow: GET, POST');
+              throw new HTTPException(405);
           }
-        }
-        return array($data, 'auth');
+        break;
         
-      case 'importer':
-        return array(array(), '../importer/templates/index');
-        
-      case 'importer-sidebar':
-        return array(array(), '../importer/templates/sidebar');
-        
-      case 'importer-extractors':
-        require __DIR__ . '/importer/lib/Importer.php';
-        $scripts = Importer::selectScripts($_REQUEST['host']);
-				return array(array_filter(array_map(array('Importer', 'loadExtractorScript'), $scripts)));
+        case 'pubmed':       
+          if (!$_GET['q']) throw new HTTPException(404, 'Not Found');
+          return array(PubMed::search($_GET['q'], $_GET['page'], $_GET['n']));
+      }
+    break;
 
-      case 'api':
-        $type = array_shift($request->parts);
-        switch ($type){
-          case 'profile':
-            return array(Mendeley::profile('me'));
-            
-          case 'folders':
-            return array(Mendeley::library_folders(), 'folders');
-
-          case 'groups':
-            return array(Mendeley::library_groups());
-
-          case 'catalog':
-            $id = array_shift($request->parts);
-            if ($id) return array(Mendeley::document($id));
-            if (!$_GET['page']) $_GET['page'] = 0;
-            if (!$_GET['n']) $_GET['n'] = 20;
-            if (!$_GET['q']) throw new HTTPException(404, 'Not Found');
-            return array(Mendeley::search('documents', $_GET['q'], $_GET['page'], $_GET['n']), 'documents');
-            
-          case 'documents':
-            $id = array_shift($request->parts);
-            switch ($_SERVER['REQUEST_METHOD']){
-              case 'GET':
-                return get_documents($id, $request);
-          
-              case 'POST':
-                return post_documents($id, $request);
-
-              default:
-                header('Allow: GET, POST');
-                throw new HTTPException(405);
-            }
-          break;
-          
-          case 'pubmed':       
-            if (!$_GET['q']) throw new HTTPException(404, 'Not Found');
-            return array(PubMed::search($_GET['q'], $_GET['page'], $_GET['n']));
-        }
-      break;
-
-      case 'appcache':
+    case 'appcache':
       header('Content-Type: text/cache-manifest');
       //header('Content-Type: text/plain');
       readfile(__DIR__ . '/static/appcache.txt');
       exit();
 
-      case 'fields':
+    case 'fields':
       $file = __DIR__ . '/static/binary/fields.js';
       header('Content-Type: application/json; charset=utf-8');
       header('Content-Length: ' . filesize($file));
       readfile($file);
       exit();
 
-      case 'sync':
+    case 'sync':
       return array(null, $request->type);
 
-      case 'annotate':
+    case 'annotate':
       $annotator = array_shift($request->parts);
       switch($annotator){
         case 'whatizit':
@@ -113,15 +112,8 @@ function route($request){
       }
       return array($data);
 
-      //default:
+    //default:
       //throw new HTTPException(404);
-    }
-  }
-  catch (HTTPException $e){
-    throw new HTTPException($e->getCode(), $e->getMessage()); // pass through
-  }
-  catch (Exception $e){
-    throw new HTTPException(500);
   }
 }
 
@@ -130,17 +122,17 @@ function get_documents($id, $request){
     $section = array_shift($request->parts);
     switch ($section){
       case 'files':
-      $filehash = array_shift($request->parts);
-      if (!$filehash) throw new HTTPException(403, 'File hash required');
-
-      Mendeley::library_readfile($id, $filehash, $_GET['group']);
-      exit();
+        $filehash = array_shift($request->parts);
+        if (!$filehash) throw new HTTPException(403, 'File hash required');
+  
+        Mendeley::library_readfile($id, $filehash, $_GET['group']);
+        exit();
 
       case '':
-      return array(Mendeley::library($id, 'documents'), 'item');
+        return array(Mendeley::library($id, 'documents'), 'item');
 
       default:
-      throw new HTTPException(404, 'Not Found');
+        throw new HTTPException(404, 'Not Found');
     }
   }
 
